c3ece604238487711e62b5323337c392872223c7
giorgia.darmiento <giorgia.darmiento@PCGIORGIA>
2022-10-10 19:31:06 +0200

    Aggiungere i file di progetto.

---
+++ ReadWriteCSVopenfile/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.RichTextBox1.EnableAutoDragDrop = True
+    End Sub
+
+    Private Sub RichTextBox1_DragDrop(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles RichTextBox1.DragDrop
+        e.Effect = DragDropEffects.None
+
+        Me.RichTextBox1.Clear()
+
+        Dim files() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+        For Each path In files
+            Me.ProcessaFile(path)
+        Next
+    End Sub
+
+    Public Sub RichTextBox1_DragEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles RichTextBox1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Sub ProcessaFile(path As String)
+        Me.RichTextBox1.AppendText(path & Environment.NewLine)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Path As String = Me.RichTextBox1.Text.Trim
+
+        Using R As New StreamReader(Path)
+
+            Do
+                Dim Line As String = R.ReadLine
+                If R.EndOfStream Then Exit Do
+
+                Me.RichTextBox2.AppendText(Line & Environment.NewLine)
+            Loop
+
+        End Using
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim o As New OpenFileDialog
+        o.ShowDialog()
+
+        If String.IsNullOrWhiteSpace(o.FileName) Then Exit Sub
+        Me.RichTextBox1.Text = o.FileName
+
+    End Sub
+End Class
---
+++ ReadWriteCSVopenfile/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Controllare i valori degli attributi degli assembly
+
+<Assembly: AssemblyTitle("ReadWriteCSVopenfile")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ReadWriteCSVopenfile")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà usato come ID del typelib
+<Assembly: Guid("3f1adf3a-27a4-4f48-bea8-03b39c65d891")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Versione principale
+'      Versione secondaria
+'      Numero di build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+' usando l'asterisco '*' come illustrato di seguito:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
